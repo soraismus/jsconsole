ae31d02747a494ee2aa841c983f6c191545d4f5e
Soraismus <tertius_lupus@yahoo.com>
2016-07-23 13:57:26 -0400

    Check in current state.

--- src/view/components/components.js
+++ src/view/components/components.js
@@ -1,30 +1,33 @@
+var PRE  = require('../../../lib/elements').PRE;
 var SPAN = require('../../../lib/elements').SPAN;
 
 function ERL_ENTRY(text) {
-  return SPAN(
-    _entryConfig,
-    SPAN(null, text + newline));
+  //return SPAN(_entryConfig, SPAN(null, text + newline));
+  return PRE(_entryConfig, text + newline);
 }
 
+/*
+// tortola does not consolidate PROMPT+INPUT+CURSOR as a distinct concept.
 function ERL_INPUT(prompt, preCursor, postCursor) {
   preCursor = preCursor != null ? preCursor : emptyString;
   postCursor = postCursor != null ? postCursor : emptyString;
   return SPAN(
     _inputConfig,
-    emptySpan,
     SPAN(
       null,
       ERL_PROMPT(prompt),
       ERL_PRE(preCursor),
       ERL_CURSOR,
-      ERL_POST(postCursor)),
-    emptySpan);
+      ERL_POST(postCursor)));
 }
+*/
 
+// Has no tortola analogue.
 function ERL_POST(text) {
   return SPAN(_postConfig, text);
 }
 
+// Corresponds to 'terminal-input'.
 function ERL_PRE(text) {
   return SPAN(_preConfig, text);
 }
@@ -39,9 +42,11 @@ var space = ' ';
 
 var emptySpan = SPAN(null, emptyString);
 
+/*
 var ERL_CURSOR = SPAN(
   {
     id: 'erl-cursor',
+    classes: { 'erl-cursor': true, 'terminal-cursor': true },
     style: {
       'background-color': '#999',
       'color': 'transparent',
@@ -51,7 +56,15 @@ var ERL_CURSOR = SPAN(
     }
   },
   space);
+*/
+var ERL_CURSOR = SPAN(
+  {
+    id: 'erl-cursor',
+    classes: { 'erl-cursor': true, 'terminal-cursor': true },
+  },
+  space);
 
+/*
 var ERL_HEADER = SPAN(
     {
       id: 'erl-header',
@@ -62,19 +75,45 @@ var ERL_HEADER = SPAN(
         style: { 'color': '#0ff' }
       },
       'Welcome to ErlKing Lisp Console.\n'));
+*/
 
 var _entryConfig = {
-  classes: { 'erl-line': true },
-  style: { 'font-weight': 'normal' }
+  classes: { 'erl-line': true, 'terminal-line': true },
+  //style: { 'font-weight': 'normal' }
 };
 
-var _inputConfig = { id: 'erl-input', style: { 'color': '#0d0' }};
-var _postConfig = { id: 'erl-post', style: { 'position': 'relative' }};
-var _preConfig = { id: 'erl-pre' };
-var _promptConfig = { id: 'erl-prompt' };
+/*
+var _inputConfig = {
+  id: 'erl-input',
+  classes: { 'erl-input': true },
+  style: { 'color': '#0d0' }
+};
+*/
+
+var _postConfig = {
+  id: 'erl-post',
+  classes: { 'erl-post': true },
+  style: { 'position': 'relative' }
+};
+
+// tortola has no concept of ERL_POST.
+var _preConfig = {
+   id: 'erl-pre',
+   classes: { 'erl-pre': true, 'terminal-input': true }
+};
+
+var _promptConfig = {
+  id: 'erl-prompt',
+  classes: { 'erl-prompt': true, 'terminal-prompt': true }
+};
 
 module.exports = {
-  ERL_ENTRY: ERL_ENTRY,
-  ERL_INPUT: ERL_INPUT,
-  ERL_HEADER: ERL_HEADER,
+  ERL_ENTRY  : ERL_ENTRY,
+  //ERL_INPUT  : ERL_INPUT,
+  //ERL_HEADER : ERL_HEADER,
+
+  ERL_PROMPT : ERL_PROMPT,
+  ERL_PRE    : ERL_PRE,
+  ERL_CURSOR : ERL_CURSOR,
+  ERL_POST   : ERL_POST
 };
--- src/view/control/recreateConsole.js
+++ src/view/control/recreateConsole.js
@@ -1,10 +1,35 @@
 var components = require('../components/components');
 var ERL_ENTRY  = components.ERL_ENTRY;
-var ERL_HEADER = components.ERL_HEADER;
-var ERL_INPUT  = components.ERL_INPUT;
+//var ERL_HEADER = components.ERL_HEADER;
+//var ERL_INPUT  = components.ERL_INPUT;
+var ERL_PROMPT = components.ERL_PROMPT;
+var ERL_PRE    = components.ERL_PRE;
+var ERL_POST   = components.ERL_POST;
+var ERL_CURSOR = components.ERL_CURSOR;
 var elements   = require('../../../lib/elements');
 var DIV        = elements.DIV;
 var PRE        = elements.PRE;
+var SECTION    = elements.SECTION;
+
+/*
+function ERLKING(prefixes, viewport) {
+  var promptLabel = prefixes.promptLabel;
+  var prompt = viewport.prompt;
+  var frame = viewport.frame;
+  var entries = viewport.terminal.entries
+    .slice(frame.start, frame.start + frame.offset)
+    .map(specifyEntry.bind(null, prefixes));
+  return DIV(
+    _erlkingConfig,
+    PRE(
+      _consoleConfig,
+      entries,
+      ERL_INPUT(
+        promptLabel,
+        prompt.preCursor,
+        prompt.postCursor)));
+}
+*/
 
 function ERLKING(prefixes, viewport) {
   var promptLabel = prefixes.promptLabel;
@@ -15,16 +40,18 @@ function ERLKING(prefixes, viewport) {
     .slice(frame.start, frame.start + frame.offset)
     .map(specifyEntry.bind(null, prefixes));
 
-  return DIV(
-    _erlkonigConfig,
-    PRE(
-      _consoleConfig,
-      ERL_HEADER,
-      entries,
-      ERL_INPUT(
-        promptLabel,
-        prompt.preCursor,
-        prompt.postCursor)));
+  return SECTION(
+    _erlkingConfig,
+    DIV(
+      null,
+      SECTION(
+        _terminalConfig,
+        DIV(
+          _terminalViewportConfig,
+          entries,
+          ERL_PROMPT(promptLabel),
+          ERL_PRE(prompt.preCursor),
+          ERL_CURSOR))));
 }
 
 function specifyEntry(prefixes, component) {
@@ -49,7 +76,12 @@ function specifyEntry(prefixes, component) {
   }
 }
 
-var _erlkingConfig = { id: 'erlking' };
+var _erlkingConfig = {
+  id: 'erlking',
+  classes: { 'erlking': true, 'container': true }
+};
 var _consoleConfig = { id: 'erl-console' };
+var _terminalConfig = { classes: { 'terminal': true }};
+var _terminalViewportConfig = { classes: { 'terminal-viewport': true }};
 
 module.exports = ERLKING;
